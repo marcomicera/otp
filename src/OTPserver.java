5326c72a79d5efb2e4d48de3f5b284da6c090bb5
Marco Micera <marco.micera@gmail.com>
2017-04-12 17:25:56 +0200

    Connessione database

--- src/OTPserver.java
+++ src/OTPserver.java
@@ -1,10 +1,16 @@
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.*;
 
 public class OTPserver extends Application {
     public void start(Stage stage) {
@@ -17,6 +23,17 @@ public class OTPserver extends Application {
                 Thread.sleep(3000); // processing
             }
         } catch(Exception e) { e.printStackTrace(); }
+
+        // Connecting to database
+        try(Connection co = DriverManager.getConnection("jdbc:mysql://localhost:3306/<database_name>?user=root&password=");
+            Statement st = co.createStatement();
+        ) {
+            ResultSet rs = st.executeQuery("select * from prova");
+            while(rs.next())
+                System.out.println(rs.getInt("<integer_column_name>") + rs.getString("<string_column_name>"));
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
 
         Group root = new Group();
         Scene scene = new Scene(root, 500, 500);
