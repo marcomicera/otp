f9cc5efd4c9e244dbdd905c5b776da96d1bcedf1
Marco Micera <marco.micera@gmail.com>
2017-04-12 12:03:30 +0200

    Adding files

---
+++ src/OTPclient.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// Package di sicurezza
+// https://docs.oracle.com/javase/7/docs/api/javax/crypto/package-summary.html
+import javax.crypto.*;
+
+public class OTPclient extends Application {
+    public void start(Stage stage) {
+        // Invio stringa
+        try(Socket s = new Socket("localhost", 8080); // Socket normale, bidirezionale
+            ObjectOutputStream oout = new ObjectOutputStream(s.getOutputStream());
+        ) { oout.writeObject("ciao, server!");
+        } catch(IOException e) { e.printStackTrace(); }
+        System.out.println("messaggio inviato");
+        
+        //  Spezzoni
+        /*private Cipher encrCipher;
+        encrCipher.getInstance("DES");
+        SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+        encrCipher.init(Cipher.ENCRYPT_MODE, key);*/
+
+        //RR
+        //RR2
+        //RR3
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500);
+        
+        stage.setTitle("One Time Password client interface");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
