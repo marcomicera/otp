3dc337049ddb83cbaf00db53aac682e1be81f135
Marco Micera <marco.micera@gmail.com>
2017-04-14 00:40:28 +0200

    Aggiunte note teoriche HOTP

--- src/OTPUI.java
+++ src/OTPUI.java
@@ -29,6 +29,32 @@ public class OTPUI {
                                         otpButton
         );
         
+        otpButton.setOnAction(
+            (ActionEvent ae) -> {
+                // HOTP generation function (to be put in a dedicated class)
+                /* RFC 4226 http://www.ietf.org/rfc/rfc4226.txt
+                    5.1.  Notation and Symbols
+                        • HOTP must be a numeric-only 6-digit value
+                        • 8-byte counter value
+                    5.3.  Generating an HOTP Value
+                        1) Generate an HMAC-SHA-1 value 
+                            • Let HS = HMAC-SHA-1(K,C)
+                            • HS is a 20-byte string
+                        2) Generate a 4-byte string (Dynamic Truncation)
+                            • Let Sbits = DT(HS)
+                            • DT, defined below, returns a 31-bit string
+                        3) Compute an HOTP value
+                            • Let Snum  = StToNum(Sbits)
+                            • Convert S to a number in 0...2^{31}-1
+                        4) Return the HOTP value
+                            • Return D = Snum mod 10^Digit
+                            • D is a number in the range 0...10^{Digit}-1
+                
+                // Practical implementation: https://goo.gl/sANcmD
+                */
+            }
+        );
+        
         setStyle();
     }
     
