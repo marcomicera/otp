a88196a3cf9542848bd02b20d81b75b0a872731d
Marco Micera <marco.micera@gmail.com>
2017-04-13 15:55:49 +0200

    SSL funzionante

--- src/OTPserver.java
+++ src/OTPserver.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
@@ -17,19 +18,50 @@ import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.net.*;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
 import javax.net.ssl.*;
 
 public class OTPserver extends Application {
-    
-private static final int PORT = 8080;
-    @Override
-    public void start(Stage stage) throws Exception {
-        // Server bidirectional socket
-        SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-        ServerSocket ss = ssf.createServerSocket(PORT);
-        Socket s = ss.accept();
-        ObjectInputStream oin = new ObjectInputStream(s.getInputStream());
-        System.out.println("Server received: " + oin.readObject());
+    public void start(Stage stage) {
+        System.setProperty("javax.net.ssl.keyStore", "mySrvKeystore");
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+        
+        SSLServerSocketFactory ssf = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+        
+        try(ServerSocket ss = ssf.createServerSocket(8080)) {
+            while(true) {
+                Socket s = ss.accept();
+
+                Thread t = new Thread() {
+                    public void run() {
+                        try(ObjectInputStream ois = new ObjectInputStream(s.getInputStream())) {
+                            SSLSession session = ((SSLSocket) s).getSession();
+                            Certificate[] cchain2 = session.getLocalCertificates();
+
+                            // Prints
+                            for (int i = 0; i < cchain2.length; i++)
+                                System.out.println(((X509Certificate) cchain2[i]).getSubjectDN());
+                            System.out.println("Peer host is " + session.getPeerHost());
+                            System.out.println("Cipher is " + session.getCipherSuite());
+                            System.out.println("Protocol is " + session.getProtocol());
+                            System.out.println("ID is " + new BigInteger(session.getId()));
+                            System.out.println("Session created in " + session.getCreationTime());
+                            System.out.println("Session accessed in " + session.getLastAccessedTime());
+                            
+                            System.out.println("Server received: " + ois.readObject());
+                            Thread.sleep(3000);
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                        
+                t.start();
+            }
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
         
         /*
         // Connecting to database
