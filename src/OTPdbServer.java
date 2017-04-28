850f22f5dfa5bb3a7e0997ee353fe6cbbb8e3e75
Marco Micera <marco.micera@gmail.com>
2017-04-28 17:04:19 +0200

    Caricamento files

---
+++ src/OTPdbServer.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OTPdbServer extends Application {
+    public void start(Stage stage) {
+        System.out.println("ciao");
+    }
+}
