d14c2ad592a75e64a42c22c33043ce4e3ea831ea
Marco Micera <marco.micera@gmail.com>
2017-04-30 21:55:05 +0200

    Ridenominazione

--- make.bat
+++ make.bat
@@ -2,5 +2,5 @@
 cls
 javac src/*.java -cp build/classes -d build/classes
 cd build/classes
-java -Djavax.net.ssl.keyStore=mySrvKeystore -Djavax.net.ssl.keyStorePassword=password OTPdbServer
+java -Djavax.net.ssl.keyStore=mySrvKeystore -Djavax.net.ssl.keyStorePassword=password OTPremoteServer
 cd ../..
--- src/OTPremoteServer.java
+++ src/OTPremoteServer.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class OTPdbServer extends Application {
+public class OTPremoteServer extends Application {
     public void start(Stage stage) {
         System.out.println("ciao");
     }
