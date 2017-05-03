477c1cc6af7bc461150ea149c7954eda79c0cd93
Riccardo <Riccardo@DESKTOP-TEHD922>
2017-05-03 21:51:53 +0200

    Aggiunto: - codice HOTP Generator - Funzione String to byte[] per la key del dongle - getCounter da foglio txt

--- src/HOTPGenerator.java
+++ src/HOTPGenerator.java
@@ -1,111 +1,220 @@
-import java.nio.ByteBuffer;
-import java.security.InvalidKeyException;
-import java.security.Key;
+/*https://tools.ietf.org/html/rfc4226#page-27*/
+ /*
+    * OneTimePasswordAlgorithm.java
+    * OATH Initiative,
+    * HOTP one-time password algorithm
+    *
+ */
+
+ /* Copyright (C) 2004, OATH.  All rights reserved.
+    *
+    * License to copy and use this software is granted provided that it
+    * is identified as the "OATH HOTP Algorithm" in all material
+    * mentioning or referencing this software or this function.
+    *
+    * License is also granted to make and use derivative works provided
+    * that such works are identified as
+    *  "derived from OATH HOTP algorithm"
+    * in all material mentioning or referencing the derived work.
+    *
+    * OATH (Open AuTHentication) and its members make no
+    * representations concerning either the merchantability of this
+    * software or the suitability of this software for any particular
+    * purpose.
+    *
+    * It is provided "as is" without express or implied warranty
+    * of any kind and OATH AND ITS MEMBERS EXPRESSaLY DISCLAIMS
+    * ANY WARRANTY OR LIABILITY OF ANY KIND relating to this software.
+    *
+    * These notices must be retained in any copies of any part of this
+    * documentation and/or software.
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
+
 import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * This class contains static methods that are used to calculate the One-Time
+ * Password (OTP) using JCE to provide the HMAC-SHA-1.
+ *
+ * @author Loren Hart
+ * @version 1.0
+ */
 public class HOTPGenerator {
-    private final String algorithm;
-    private final int passwordLength;
-    private final int modDivisor;
 
-    public static final int DEFAULT_PASSWORD_LENGTH = 6; // decimal digits
-    public static final String HOTP_HMAC_ALGORITHM = "HmacSHA1"; // specified by the HOTP standard RFC 4226
-
-    public HOTPGenerator() throws NoSuchAlgorithmException {
-        this(DEFAULT_PASSWORD_LENGTH);
+    HOTPGenerator() {
     }
 
-    public HOTPGenerator(final int passwordLength) throws NoSuchAlgorithmException {
-        this(passwordLength, HOTP_HMAC_ALGORITHM);
-    }
+    // These are used to calculate the check-sum digits.
+    //                                0  1  2  3  4  5  6  7  8  9
+    private static final int[] doubleDigits
+            = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
 
     /**
-     * <p>Creates a new HMAC-based one-time password generator using the given password length and algorithm. Note that
-     * <a href="https://tools.ietf.org/html/rfc4226">RFC&nbsp;4226</a> specifies that HOTP must always use HMAC-SHA1 as
-     * an algorithm, but derived one-time password systems like TOTP may allow for other algorithms.</p>
+     * Calculates the checksum using the credit card algorithm. This algorithm
+     * has the advantage that it detects any single mistyped digit and any
+     * single transposition of adjacent digits.
      *
-     * @param passwordLength the length, in decimal digits, of the one-time passwords to be generated; must be between
-     * 6 and 8, inclusive
-     * @param algorithm the name of the {@link javax.crypto.Mac} algorithm to use when generating passwords; note that
-     * HOTP only allows for {@value HOTPGenerator#HOTP_HMAC_ALGORITHM}, but derived
-     * standards like TOTP may allow for other algorithms
+     * @param num the number to calculate the checksum for
+     * @param digits number of significant places in the number
      *
-     * @throws NoSuchAlgorithmException if the given algorithm is not supported by the underlying JRE
+     * @return the checksum of num
      */
-    protected HOTPGenerator(final int passwordLength, final String algorithm) throws NoSuchAlgorithmException {
-        switch(passwordLength) {
-            case 6: {
-                this.modDivisor = 1_000_000;
-                break;
+    public static int calcChecksum(long num, int digits) {
+        boolean doubleDigit = true;
+        int total = 0;
+        while (0 < digits--) {
+            int digit = (int) (num % 10);
+            num /= 10;
+            if (doubleDigit) {
+                digit = doubleDigits[digit];
             }
-
-            case 7: {
-                this.modDivisor = 10_000_000;
-                break;
-            }
-
-            case 8: {
-                this.modDivisor = 100_000_000;
-                break;
-            }
-
-            default: {
-                throw new IllegalArgumentException("Password length must be between 6 and 8 digits.");
-            }
+            total += digit;
+            doubleDigit = !doubleDigit;
         }
-
-        this.passwordLength = passwordLength;
-
-        // Our purpose here is just to throw an exception immediately if the algorithm is bogus.
-        Mac.getInstance(algorithm);
-        this.algorithm = algorithm;
+        int result = total % 10;
+        if (result > 0) {
+            result = 10 - result;
+        }
+        return result;
     }
 
     /**
-     * Generates a one-time password using the given key and counter value.
+     * This method uses the JCE to provide the HMAC-SHA-1 algorithm. HMAC
+     * computes a Hashed Message Authentication Code and in this case SHA1 is
+     * the hash algorithm used.
      *
-     * @param key a secret key to be used to generate the password
-     * @param counter the counter value to be used to generate the password
+     * @param keyBytes the bytes to use for the HMAC-SHA-1 key
+     * @param text the message or text to be authenticated.
      *
-     * @return an integer representation of a one-time password; callers will need to format the password for display
-     * on their own
+     * @throws NoSuchAlgorithmException if no provider makes either HmacSHA1 or
+     * HMAC-SHA-1 digest algorithms available.
+     * @throws InvalidKeyException The secret provided was not a valid
+     * HMAC-SHA-1 key.
      *
-     * @throws InvalidKeyException if the given key is inappropriate for initializing the {@link Mac} for this generator
      */
-    public int generateOneTimePassword(final Key key, final long counter) throws InvalidKeyException {
-        final Mac mac;
-
+    public static byte[] hmac_sha1(byte[] keyBytes, byte[] text)
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        //        try {
+        Mac hmacSha1;
         try {
-            mac = Mac.getInstance(this.algorithm);
-            mac.init(key);
-        } catch (final NoSuchAlgorithmException e) {
-            // This should never happen since we verify that the algorithm is legit in the constructor.
-            throw new RuntimeException(e);
+            hmacSha1 = Mac.getInstance("HmacSHA1");
+        } catch (NoSuchAlgorithmException nsae) {
+            hmacSha1 = Mac.getInstance("HMAC-SHA-1");
+        }
+        SecretKeySpec macKey
+                = new SecretKeySpec(keyBytes, "RAW");
+        hmacSha1.init(macKey);
+        return hmacSha1.doFinal(text);
+        //        } catch (GeneralSecurityException gse) {
+        //            throw new UndeclaredThrowableException(gse);
+        //        }
+    }
+
+    private static final int[] DIGITS_POWER
+            // 0 1  2   3    4     5      6       7        8
+            = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+
+    /**
+     * This method generates an OTP value for the given set of parameters.
+     *
+     * @param secret the shared secret
+     * @param movingFactor the counter, time, or other value that changes on a
+     * per use basis.
+     * @param codeDigits the number of digits in the OTP, not including the
+     * checksum, if any.
+     * @param addChecksum a flag that indicates if a checksum digit should be
+     * appended to the OTP.
+     * @param truncationOffset the offset into the MAC result to begin
+     * truncation. If this value is out of the range of 0 ... 15, then dynamic
+     * truncation will be used. Dynamic truncation is when the last 4 bits of
+     * the last byte of the MAC are used to determine the start offset.
+     * @throws NoSuchAlgorithmException if no provider makes either HmacSHA1 or
+     * HMAC-SHA-1 digest algorithms available.
+     * @throws InvalidKeyException The secret provided was not a valid
+     * HMAC-SHA-1 key.
+     *
+     * @return A numeric String in base 10 that includes {@link codeDigits}
+     * digits plus the optional checksum digit if requested.
+     */
+    static public String generateOTP(byte[] secret, long movingFactor, int codeDigits, boolean addChecksum, int truncationOffset)
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        // put movingFactor value into text byte array
+        String result = null;
+        int digits = addChecksum ? (codeDigits + 1) : codeDigits;
+        byte[] text = new byte[8];
+        for (int i = text.length - 1; i >= 0; i--) {
+            text[i] = (byte) (movingFactor & 0xff);
+            movingFactor >>= 8;
         }
 
-        final ByteBuffer buffer = ByteBuffer.allocate(8);
-        buffer.putLong(0, counter);
+        // compute hmac hash
+        byte[] hash = hmac_sha1(secret, text);
 
-        final byte[] hmac = mac.doFinal(buffer.array());
-        final int offset = hmac[hmac.length - 1] & 0x0f;
-
-        for(int i = 0; i < 4; i++) {
-            // Note that we're re-using the first four bytes of the buffer here; we just ignore the latter four from
-            // here on out.
-            buffer.put(i, hmac[i + offset]);
+        // put selected bytes into result int
+        int offset = hash[hash.length - 1] & 0xf;
+        if ((0 <= truncationOffset)
+                && (truncationOffset < (hash.length - 4))) {
+            offset = truncationOffset;
         }
+        int binary
+                = ((hash[offset] & 0x7f) << 24)
+                | ((hash[offset + 1] & 0xff) << 16)
+                | ((hash[offset + 2] & 0xff) << 8)
+                | (hash[offset + 3] & 0xff);
 
-        final int hotp = buffer.getInt(0) & 0x7fffffff;
-
-        return hotp % this.modDivisor;
+        int otp = binary % DIGITS_POWER[codeDigits];
+        if (addChecksum) {
+            otp = (otp * 10) + calcChecksum(otp, codeDigits);
+        }
+        result = Integer.toString(otp);
+        while (result.length() < digits) {
+            result = "0" + result;
+        }
+        return result;
+    }
+    
+    //****************************************************************************************************************************
+    //Source: http://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java
+    public static byte[] hexStringToByteArray(String s) {
+    int len = s.length();
+    byte[] data = new byte[len / 2];
+    for (int i = 0; i < len; i += 2) {
+        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                             + Character.digit(s.charAt(i+1), 16));
+    }
+    return data;
     }
 
-    public int getPasswordLength() {
-        return this.passwordLength;
+    public static long getCounter() throws IOException {     
+            BufferedReader br = new BufferedReader(new InputStreamReader(OTPclient.class.getResourceAsStream("/counter.txt")));
+            String line;
+            long result = 1;
+            while ((line = br.readLine()) != null) {
+                result = Long.parseLong(line);
+            }
+            return result;
     }
 
-    public String getAlgorithm() {
-        return this.algorithm;
+    public static void updateCounter(long counter) throws FileNotFoundException, IOException {
+        String counterStr = Long.toString(counter);
+        try {
+            BufferedWriter out;
+            out = new BufferedWriter(new FileWriter("/counter.txt"));
+            out.write(counterStr);  
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Exception "+ e);
+
+        }
     }
+    //****************************************************************************************************************************
 }
