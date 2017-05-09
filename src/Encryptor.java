61292f55f6a086828d2788bdf61b3d0a1f86adea
Marco Micera <marco.micera@gmail.com>
2017-05-09 23:30:37 +0200

    Non fa il decrypting

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -1,63 +1,54 @@
+import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-
-// http://stackoverflow.com/questions/15554296/simple-java-aes-encrypt-decrypt-example
 
 public class Encryptor {
-    // private static final String ENCR_ALGORITHM = "AES"; // blocks of 16B
-    private SecretKey key;
-    
+    private static final String ALGORITHM = "AES";
+    private byte[] key;
+
     public Encryptor() {
         try {
-            key = KeyGenerator.getInstance("AES").generateKey();
+            key = KeyGenerator.getInstance(Encryptor.ALGORITHM).generateKey().getEncoded();
         } catch(NoSuchAlgorithmException ex) {
             Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public String encrypt(String initVector, String value) {
+
+    public byte[] encrypt(byte[] plainText) {
+        SecretKeySpec secretKey = new SecretKeySpec(key, Encryptor.ALGORITHM);
+        
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-
-            byte[] encrypted = cipher.doFinal(value.getBytes());
-            System.out.println("encrypted string:" + DatatypeConverter.printBase64Binary(encrypted));
-
-            return DatatypeConverter.printBase64Binary(encrypted);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            Cipher cipher = Cipher.getInstance(Encryptor.ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            
+            return cipher.doFinal(plainText);
+        } catch(GeneralSecurityException gse) {
+            Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, gse);
         }
-
+        
         return null;
     }
 
-    public String decrypt(String initVector, String encrypted) {
+    public byte[] decrypt(byte[] cipherText) {
+        SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
+
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-
-            byte[] original = cipher.doFinal(
-                DatatypeConverter.parseBase64Binary(encrypted‌​)
-            );
-
-            return new String(original);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            
+            return cipher.doFinal(cipherText);
+        } catch(GeneralSecurityException gse) {
+            Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, gse);
         }
-
+        
         return null;
     }
 }
