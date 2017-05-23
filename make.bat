a2650028698182bf08921bb0076019fcebe03d5b
Marco Micera <marco.micera@gmail.com>
2017-05-23 17:34:23 +0200

    Makefile copy

--- make.bat
+++ make.bat
@@ -2,6 +2,6 @@
 cls
 javac src/*.java -cp build/classes;lib -d build/classes
 cd build/classes
-xcopy CounterResponse.class ../../../OTPlocalServer/build/classes
+copy CounterResponse.class ../../../OTPlocalServer/build/classes
 java  -Djavax.net.ssl.keyStore=../../remoteServerCertificate -Djavax.net.ssl.keyStorePassword=password -Djavax.net.ssl.trustStore=../../localServerCertificate -Djavax.net.ssl.trustStorePassword=password -cp .;../../lib/mysql-connector-java-5.1.42-bin.jar OTPremoteServer
 cd ../..
