ecbc98ce592054698b6fd217b6364015365a9257
Riccardo <Riccardo@DESKTOP-TEHD922>
2017-05-23 17:48:16 +0200

    Aggiunto:  - encryption e decryption di lo

---
+++ src/CounterResponse.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+ 
+public class CounterResponse implements Serializable {
+    private Long dongle_counter;
+    private String dongle_key;
+
+    protected CounterResponse(Long c, String k) {
+        dongle_counter = c;
+        dongle_key = k;
+    }
+
+    @Override
+    public String toString() {
+        return "[counter: " + dongle_counter + " | key: " + dongle_key + "]";
+    }
+    
+    public Long getDongleCounter() { return dongle_counter; }
+    public String getDongleKey() { return dongle_key; }
+}
